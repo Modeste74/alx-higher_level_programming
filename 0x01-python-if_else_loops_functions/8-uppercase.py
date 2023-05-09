0185f3371f0888f35000ba8b2246f4689e5f44a8
Modeste Ciira <ciiramodeste23@gmail.com>
2023-05-09 19:19:09 +0300

    task done

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    convert = ""
+    for lower in str:
+        if 97 <= ord(lower) <= 122:
+            convert += chr(ord(lower) - 32)
+        else:
+            convert += lower
+    print(convert)
