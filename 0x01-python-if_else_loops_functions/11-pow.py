0de93cdb6c9f1b792699b46317a36e2e84c12249
Modeste Ciira <ciiramodeste23@gmail.com>
2023-05-09 23:03:59 +0300

    task done

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result = 1
-    if b == 0:
-        return 1
-    elif b > 0:
-        for n in range(b):
-            result *= a
-    else:
-        for n in range(abs(b)):
-            result *= 1/a
-    return result
+    return a ** b
