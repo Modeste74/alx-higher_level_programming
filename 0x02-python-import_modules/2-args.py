3323dfab65d3146536f9030164e813bf320ddf2c
Modeste Ciira <ciiramodeste23@gmail.com>
2023-05-11 15:23:26 +0300

    Task done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    args = sys.argv[1:]
-    num_arg = len(args)
-    if num_arg == 0:
-        print("No arguments.")
-    elif num_arg == 1:
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(num_arg))
-    for i, arg in enumerate(args):
-        print("{}: {}".format(i + 1, arg))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
