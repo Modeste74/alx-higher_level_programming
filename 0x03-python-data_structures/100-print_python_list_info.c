28104a2e25eaafd3f3d8d7b55dc4b2209ebea586
Modeste Ciira <ciiramodeste23@gmail.com>
2023-05-15 14:04:32 +0300

    task done

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,4 @@
-#include "/usr/include/python3.4/Python.h"
-#include "/usr/include/python3.4/listobject.h"
-#include "/usr/include/python3.4/object.h"
+#include <Python.h>
 #include <stdio.h>
 void print_python_list_info(PyObject *p)
 {
