1ff5eb6b60227f4e6509ada22fca6de81591658c
Modeste Ciira <ciiramodeste23@gmail.com>
2023-05-15 12:38:12 +0300

    task done

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     n = len(my_list)
-    if idx < 0:
-        return None
-    elif idx > n:
+    if idx < 0 or idx >= n:
         return None
     else:
         return my_list[idx]
