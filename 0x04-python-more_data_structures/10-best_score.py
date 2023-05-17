11164bfd697afcef7acf5b2f64315a03389b7da5
Modeste Ciira <ciiramodeste23@gmail.com>
2023-05-17 17:02:27 +0300

    Task done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    else:
-        return max(a_dictionary.keys())
+    max_key = None
+    max_value = float('-inf')
+    for n, m in a_dictionary.items():
+        if m > max_value:
+            max_key = n
+            max_value = m
+    return max_key
