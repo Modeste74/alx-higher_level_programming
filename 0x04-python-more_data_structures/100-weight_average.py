79d8b06c0ed638ab65d2a9e8253da6c7b4983981
Modeste Ciira <ciiramodeste23@gmail.com>
2023-05-18 21:47:13 +0300

    Task done

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    t_sum = 0
+    t_weight = 0
+    for score, weight in my_list:
+        t_sum += score * weight
+        t_weight += weight
+    av_weight = t_sum / t_weight
+    return av_weight
