73d778bb0b1d0fb71bca035a5db37318d2cc8a60
Modeste Ciira <ciiramodeste23@gmail.com>
2023-05-30 23:01:28 +0300

    Task done

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 """Define a class Square"""
+
 class Square:
     """Representation of a square
-    with private attribute size"""
+    with private attribute size 
+    """
+
     def __init__(self, size):
         """Initialize the square with the
-        args: size which is the size ofthe square
+
+        Args:
+            size(int): which is the size ofthe square
+
+        Return:
+            None
+
         """
         self.__size = size
