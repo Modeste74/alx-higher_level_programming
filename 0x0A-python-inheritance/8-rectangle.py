641be49590387c9e9689106e958c07be97496727
Modeste Ciira <ciiramodeste23@gmail.com>
2023-06-12 16:32:21 +0300

    Task done

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""define a class BaseGeometry"""
+"""define a class Rectangle that
+inherits the attributes of
+BaseGeometry
+"""
 
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """Reps a rectangle"""
     def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
+        """intializes the rectangle"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """define a class BaseGeometry"""
 
-
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """represents a rectangle"""
     def __init__(self, width, height):
+        """intializes the rectangle"""
         self.__width = width
         self.__height = height
         self.integer_validator("width", width)
