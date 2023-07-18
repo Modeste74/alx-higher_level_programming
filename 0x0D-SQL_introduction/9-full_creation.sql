2633eecd2de2fcf6944d08ade8d06eae6f85afaa
Modeste Ciira <ciiramodeste23@gmail.com>
2023-07-18 21:08:17 +0300

    fixed task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS second_table(
 	name VARCHAR(256),
 	score INT
 	);
-INSERT INTO hbtn_0c_0.second_table (id, name, score)
+INSERT INTO second_table (id, name, score)
 VALUES
 (1, 'John', 10)
 (2, 'Alex', 3)
