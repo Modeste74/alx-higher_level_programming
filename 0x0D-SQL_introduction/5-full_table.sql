8d99099bd7f88e29fd6d650fc746478471157d16
Modeste Ciira <ciiramodeste23@gmail.com>
2023-07-18 22:29:48 +0300

    error fixed

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,4 @@
 -- prints the full description of the table first_table
 -- from the database hbtn_0c_0 in your MySQL server
 
-SELECT *
-FROM INFROMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
