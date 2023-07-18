7df5d2ea6fe8739be5ddce8fdef7114d8c2b2c95
Modeste Ciira <ciiramodeste23@gmail.com>
2023-07-18 21:01:29 +0300

    fixed task 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,7 +1,7 @@
 -- creates a table called first_table in the
 -- current database in your MySQL server
 
-CREATE TABLES IF NOT EXISTS first_table(
+CREATE TABLE IF NOT EXISTS first_table(
 	id INT,
 	name VARCHAR(256)
 	);
