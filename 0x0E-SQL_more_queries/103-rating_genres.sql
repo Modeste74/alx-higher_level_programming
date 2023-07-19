99ff9dc33420acb4cdd7aafd411ca7bf75a370ef
Modeste Ciira <ciiramodeste23@gmail.com>
2023-07-19 23:58:58 +0300

    fixed

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -5,6 +5,7 @@
 --JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id
 --JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
 --GROUP BY tv_genres.id
+
 SELECT tv_genres.name, rating_sum
 FROM tv_genres
 LEFT JOIN (
