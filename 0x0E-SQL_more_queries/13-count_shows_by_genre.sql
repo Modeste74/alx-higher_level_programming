d660d5070ee90e6ec37d494d517cfbac5ae0b6ae
Modeste Ciira <ciiramodeste23@gmail.com>
2023-07-20 00:33:14 +0300

    change made

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,6 +2,7 @@
 
 SELECT tv_show_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_show_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
 HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
