2a3be75cc247e3347f47a3014767007c63d0b528
Modeste Ciira <ciiramodeste23@gmail.com>
2023-07-19 12:58:57 +0300

    task 0 fixed

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- lists all privilleges of the certain mysql users
 
-SHOW GRANT FOR 'user_0d_1'@'localhost';
-SHOW GRANT FOR 'user_0d_2'@'localhost';
+SHOW GRANT FOR user_0d_1@localhost;
+SHOW GRANT FOR user_0d_2@localhost;
