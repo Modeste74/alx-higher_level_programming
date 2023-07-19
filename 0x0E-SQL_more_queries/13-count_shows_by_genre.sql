d61eaf2dd516264485e53d5c15fa58f94126cbcb
Modeste Ciira <ciiramodeste23@gmail.com>
2023-07-19 22:18:08 +0300

    Number of shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- list all genre and display no of shows linked
 
-SELECT tv_show_genres.genre AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+SELECT tv_show_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_show_genres
 GROUP BY genre
 HAVING number_of_shows > 0
