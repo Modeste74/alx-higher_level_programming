ceb575eea404da21fb6aaf13f9ae8554effae970
Modeste Ciira <ciiramodeste23@gmail.com>
2023-07-19 23:22:50 +0300

    No Comedy tonight!

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,3 +1,11 @@
 -- list all show without a specific genre
 
-
+SELECT title
+FROM tv_shows
+WHERE id NOT IN(
+	SELECT tv_show_id
+	FROM tv_show_genres
+	JOIN tv_genres ON tv_show_genres.show_id = tv_genres.id
+	WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY title ASC;
