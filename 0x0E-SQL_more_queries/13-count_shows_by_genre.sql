e153ff751d4243d07f15a3158ca6b0be4322b852
Modeste Ciira <ciiramodeste23@gmail.com>
2023-07-19 22:03:08 +0300

    Number of shows by genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- list all genre and display no of shows linked
+
+SELECT tv_show_genres.genre AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_show_genres
+GROUP BY genre
+HAVING number_of_shows > 0
+ORDER BY number_of_shows DESC;
