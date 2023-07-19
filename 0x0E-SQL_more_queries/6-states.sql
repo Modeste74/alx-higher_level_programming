78fd9e09359c4229d65ddc6debff9f845ed43a84
Modeste Ciira <ciiramodeste23@gmail.com>
2023-07-19 15:13:12 +0300

    States table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- creates a database and a table
+-- in the table there should be a field that is unique 
+-- should be auto-generated and is a primary key
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+	id INT NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (id),
+	name VARCHAR(256) NOT NULL
+	);
