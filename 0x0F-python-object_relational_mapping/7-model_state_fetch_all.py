2816f9c5122ba121c3f5e64f385c32b3251044bf
Modeste Ciira <ciiramodeste23@gmail.com>
2023-08-18 20:43:19 +0300

    trying something

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""list all states in the table"""
+"""
+lists all State objects from a database
+"""
 
-import sys
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sys import argv
 from model_state import Base, State
 
-
 if __name__ == "__main__":
-    engine = create_engine(
-            f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}"
-            "@localhost/{sys.argv[3]}", pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
     session = Session()
-    states = session.query(State).order_by(State.id)
-    for state in states:
+    for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
     session.close()
---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all_1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""list all states in the table"""
+
+import sys
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+            f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}"
+            "@localhost/{sys.argv[3]}", pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id)
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
