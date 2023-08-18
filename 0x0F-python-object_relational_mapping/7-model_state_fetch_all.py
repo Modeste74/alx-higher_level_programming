d82cbd8aafb92b85b3a2000c082aa0a16a65dab8
Modeste Ciira <ciiramodeste23@gmail.com>
2023-08-18 20:53:57 +0300

    trying something

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,21 +1,42 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-lists all State objects from a database
+Script to list all State objects from a MySQL database.
 """
 
-import sqlalchemy
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
 from model_state import Base, State
 
-if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                    argv[2],
-                                                                    argv[3]))
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
+
+def list_states(mysql_username, mysql_password, db_name):
+    """Connects to the database and lists all State objects."""
+
+    # Create an engine to connect to the MySQL server
+    engine = create_engine(
+        f'mysql+mysqldb://{mysql_username}:{mysql_password}'
+        '@localhost:3306/{db_name}',
+        pool_pre_ping=True)
+
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
+
+    # Retrieve and list all State objects sorted by id
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    # Close the session
     session.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+    else:
+        mysql_username = sys.argv[1]
+        mysql_password = sys.argv[2]
+        db_name = sys.argv[3]
+        list_states(mysql_username, mysql_password, db_name)
