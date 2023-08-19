73043be63c602ae575fc091c8b9b4ff415952b81
Modeste Ciira <ciiramodeste23@gmail.com>
2023-08-19 21:58:51 +0300

    corrections made

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -5,7 +5,8 @@ the database hbtn_0e_101_usa"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State, City
+from relationship_state import Base, State
+from relationship_city import City
 
 
 if __name__ == "__main__":
