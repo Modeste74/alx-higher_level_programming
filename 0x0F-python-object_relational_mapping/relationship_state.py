d3157f106a5c8b9ab7d73da49e97b89fde33d00b
Modeste Ciira <ciiramodeste23@gmail.com>
2023-08-19 21:37:56 +0300

    error fixed

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -3,7 +3,7 @@
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchmey.orm import relationship
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
