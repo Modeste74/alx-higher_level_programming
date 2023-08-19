eb8d82c90bc17253ffc392350cf16bbd55459ff1
Modeste Ciira <ciiramodeste23@gmail.com>
2023-08-19 22:44:54 +0300

    testing something

---
+++ 0x0F-python-object_relational_mapping/testing.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""lists all State objects, and corresponding
+City objects, contained in the database
+hbtn_0e_101_usa"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+
+if __name__ == "__main__":
+    db_username = sys.argv[1]
+    db_password = sys.argv[2]
+    db_database = sys.argv[3]
+    engine = create_engine(
+            f"mysql+mysqldb://{db_username}:{db_password}"
+            "@localhost:3306/{db_database}", pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"{city.id}: {city.name}")
+    session.close()
