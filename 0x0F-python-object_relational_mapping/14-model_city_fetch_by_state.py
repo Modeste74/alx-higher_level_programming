7ee6190d9816250436e44e0b54df15723d94dda6
Modeste Ciira <ciiramodeste23@gmail.com>
2023-08-20 22:40:18 +0300

    corrections made

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -21,5 +21,5 @@ if __name__ == "__main__":
     cities = session.query(City, State.name).join(
             State).order_by(City.id).all()
     for city, state in cities:
-        print(f"{state_name}: ({city.id}) {city.name}")
+        print(f"{state.name}: ({city.id}) {city.name}")
     session.close()
