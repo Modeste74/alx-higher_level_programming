39e11f0a1f3ce3da01cea111948ddb8845cee177
Modeste Ciira <ciiramodeste23@gmail.com>
2023-08-20 12:38:07 +0300

    corrections made

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,30 +5,6 @@ import MySQLdb
 import sys
 
 
-def list_state(username, password, database, state_name):
-    """displays all values in the states table
-    of hbtn_0e_0_usa where name matches the argument.
-    """
-    try:
-        db = MySQLdb.connect(
-                host="localhost",
-                port=3306,
-                user=username,
-                passwd=password,
-                db=database
-                )
-        cursor = db.cursor()
-        query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id"
-        cursor.execute(query, (state_name,))
-        states = cursor.fetchall()
-        for state in states:
-            print(state)
-        db.close()
-    except MySQLdb.Error as e:
-        print(f"Error: {e}")
-        sys.exit(1)
-
-
 if __name__ == "__main__":
     if len(sys.argv) != 5:
         sys.exit(1)
@@ -37,5 +13,17 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
     state_name = sys.argv[4]
-
-    list_state(username, password, database, state_name)
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id"
+    cursor.execute(query, (state_name,))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    db.close()
