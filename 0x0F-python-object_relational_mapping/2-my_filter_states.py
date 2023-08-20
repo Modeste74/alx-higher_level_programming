f2dc645196a3ce6c77df6b7be5d94e668e2f0478
Modeste Ciira <ciiramodeste23@gmail.com>
2023-08-20 13:18:24 +0300

    corrections made

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-""" lists all states from the database hbtn_0e_0_usa """
+"""defines a function list_state"""
 
-if __name__ == '__main__':
-    import sys
+
+if __name__ == "__main__":
     import MySQLdb
+    import sys
 
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    host = 'localhost'
-
-    db = MySQLdb.connect(host=host, user=mysql_username, passwd=mysql_password,
-                         db=database_name, port=3306)
-
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
     cursor = db.cursor()
-    query = 'SELECT * FROM states WHERE name LIKE BINARY "{}"\
- ORDER BY id ASC'.format(sys.argv[4])
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}'\
+            ORDER BY id".format(sys.argv[4])
     cursor.execute(query)
-
-    rows = cursor.fetchall()
-
-    for row in rows:
-        print(row)
-
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
     cursor.close()
     db.close()
