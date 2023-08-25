f56880c1d4f5f49d266147bfcfe9ff35fabfc8ee
Modeste Ciira <ciiramodeste23@gmail.com>
2023-08-25 21:14:07 +0300

    error fixed

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     first_state = session.query(State).order_by(State.id).first()
-    if first_state is not None:
+    if first_state:
         print(f"{first_state.id}: {first_state.name}")
     else:
         print("Nothing")
