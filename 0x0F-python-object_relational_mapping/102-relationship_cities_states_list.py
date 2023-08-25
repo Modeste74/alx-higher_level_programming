8a62b822dc112585f89d1f070067b13360ed13ad
Modeste Ciira <ciiramodeste23@gmail.com>
2023-08-25 22:43:03 +0300

    code fixed

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     engine = create_engine(
             f"mysql+mysqldb://{db_username}:{db_password}\
                     @localhost:3306/{db_database}", pool_pre_ping=True)
-    Base/metadata.create_all(engine)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     cities = session.query(City).order_by(City.id).all()
